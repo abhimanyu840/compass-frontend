87a94a95580c6c3cc66a8c0f69655d85e9dad517
abhimanyu840 <akabhimanyukumar111@gmail.com>
2024-03-18 20:31:10 +0530

    design change 1

--- src/components/consumer/Home.jsx
+++ src/components/consumer/Home.jsx
@@ -6,6 +6,7 @@ import apiContext from '../context/apiContext'
 const Home = ({ admin }) => {
     return (
         <>
+            <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
             <div className='container font-poppins mx-4 '>
                 <section className='text-center my-4 mx-2'>
 
